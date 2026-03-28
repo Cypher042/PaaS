728d6ad5918df559f704c7796bdc94707bbc8a3c
Cypher042 <ayush200501@gmail.com>
2026-03-28 06:12:45 +0530

    [user-service] feat : Added Support for MongoDB

---
+++ apps/user-service/internal/database/database.go
@@ -0,0 +1,24 @@
+package database
+
+import (
+	"context"
+	"log"
+
+	"go.mongodb.org/mongo-driver/v2/mongo"
+	"go.mongodb.org/mongo-driver/v2/mongo/options"
+)
+
+func Connect(MONGODB_URI string) *mongo.Database {
+
+	clientOptions := options.Client().ApplyURI(MONGODB_URI)
+
+	client, err := mongo.Connect(clientOptions)
+	if err != nil {
+		log.Fatalf("error connecting to MongoDB: %v", err)
+	}
+
+	defer client.Disconnect(context.TODO())
+
+	return client.Database("PaaS")
+
+}
