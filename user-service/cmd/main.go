8ecb442525fc5d73c22f9e22241b5677f2251673
Anubhaw6557 <aanubhaw0@gmail.com>
2026-03-31 20:17:49 +0530

    some_fixes

--- user-service/cmd/main.go
+++ user-service/cmd/main.go
@@ -1,10 +1,26 @@
 package main
 
 import (
+	"log"
+	"os"
+
+	"github.com/Cypher042/PaaS/user-service/internal/database"
+	"github.com/Cypher042/PaaS/user-service/internal/repository"
 	"github.com/Cypher042/PaaS/user-service/internal/router"
+	"github.com/Cypher042/PaaS/user-service/internal/user"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("%v",err)
+	}
+	database := database.Connect(os.Getenv("MONGODB_URI"))
+	user_repo := repository.UserRepo {
+		Collection: database.Collection("user-service"),
+	}
+	user_service := user.NewService(&user_repo)
 	r := router.SetupRouter()
 	// Listen and Server in 0.0.0.0:8080
 	_ = r.Run(":8080")
